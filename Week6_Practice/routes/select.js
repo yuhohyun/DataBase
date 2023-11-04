fd00b349357ac58de039421cc44afa31dc69c548
Hohyun <yuhohyun@naver.com>
2023-11-04 11:06:53 +0900

    modify select page

--- Week6_Practice/routes/select.js
+++ Week6_Practice/routes/select.js
@@ -4,24 +4,12 @@ import { selectSql } from '../database/sql';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const Building = await selectSql.getBuilding();
-    const Department = await selectSql.getDepartment();
-    const Room = await selectSql.getRoom();
     const Student = await selectSql.getStudent();
-    const Class = await selectSql.getClass();
 
     res.render('select', {
         main_title: "Tables in InhaDB",
-        title1: "Building",
-        title2: "Department",
-        title3: "Room",
-        title4: "Student",
-        title5: "Class",
-        Building,
-        Department,
-        Room,
+        title1: "Student",
         Student,
-        Class,
     })
 })
 
